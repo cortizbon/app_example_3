0ebf7e913199769093f6e057739ef59c407f8877
Carlos Ortiz <67929766+cortizbon@users.noreply.github.com>
2023-07-27 14:59:47 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title("Primera aplicación en streamlit")
+
+X = np.random.normal(0, 10, 1000)
+Y = np.random.normal(5, 10, 1000)
+
+fig, ax = plt.subplots(1, 2, sharex=True)
+
+ax[0].hist(X, bins=50)
+ax[1].hist(Y, bins=50)
+
+st.pyplot(fig)
